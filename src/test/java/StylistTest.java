44444fe894137597086d088e0494a263961adaba
slaughtr <dslaughtr@gmail.com>
2017-03-31 08:30:30 -0700

    Add DatabaseRules.java

--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -4,17 +4,8 @@ import static org.junit.Assert.*;
 
 public class StylistTest {
 
-  @Before
-  public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
-  }
+  @Rule
+    public DatabaseRule database = new DatabaseRule();
 
-  @After
-  public void tearDown() {
-    try (Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM name_of_your_table *;";
-      con.createQuery(sql).executeUpdate();
-    }
-  }
 
 }
