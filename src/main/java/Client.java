1a1d8a0fc1be3a9ee48733bf33da201cea579cac
slaughtr <dslaughtr@gmail.com>
2017-03-31 08:51:43 -0700

    Add client ID test, save/find/all methods to client.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,16 +1,74 @@
 import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Client {
   private String name;
   private String appointment;
-
   private int id;
   private int stylistId;
 
   public Client(String name, String appointment) {
     this.name = name;
     this.appointment = appointment;
-    
+
+  }
+
+  public String getClientName() {
+    return name;
+  }
+
+  public String getClientAppointment() {
+    return appointment;
+  }
+
+  public int getClientId() {
+    return id;
+  }
+
+
+
+
+  public static List<Client> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM clients";
+      return con.createQuery(sql)
+      .executeAndFetch(Client.class);
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO clients (name, appointment) VALUES (:name, CAST(:appointment AS DATE))";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", this.name)
+      .addParameter("appointment", this.appointment)
+      // .addParameter("stylist_id", this.stylistId)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  public static Client find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM clients WHERE id = :id";
+      Client patient = con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetchFirst(Client.class);
+      return patient;
+    }
+  }
+
+
+  @Override
+  public boolean equals(Object otherClient){
+    if (!(otherClient instanceof Client)){
+      return false;
+    } else {
+      Client newClient = (Client) otherClient;
+      return this.getClientName().equals(newClient.getClientName()) && this.getClientId() == newClient.getClientId();
+    }
   }
 
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,6 +1,9 @@
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ClientTest {
 
@@ -12,6 +15,25 @@ public class ClientTest {
   public void client_instantiatesCorrectly_true() {
     Client testClient = new Client("Person", "1999-01-01");
     assertTrue(testClient instanceof Client);
+  }
+
+  @Test
+  public void client_instantiatesWithName_true() {
+    Client testClient = new Client("Person", "1999-01-01");
+    assertTrue(testClient.getClientName().equals("Person"));
+  }
+
+  @Test
+  public void client_instantiatesWithAppointment_true() {
+    Client testClient = new Client("Person", "1999-01-01");
+    assertTrue(testClient.getClientAppointment().equals("1999-01-01"));
+  }
+
+  @Test
+  public void client_instantiatesWithId_true() {
+    Client testClient = new Client("Person", "1999-01-01");
+    testClient.save();
+    assertTrue(testClient.getClientId() > 0);
   }
 
 
