0172b8bbde9e00692bdd69d85f6d6ebdc6d589d7
slaughtr <dslaughtr@gmail.com>
2017-03-31 10:08:36 -0700

    Add navbar to layou.vtl, basic clients and stylists pages

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,6 +9,23 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
+    // Stylist testStylist1 = new Stylist("Judy", "Judgement");
+    // testStylist1.save();
+    // Client testClient = new Client("Joe Schmo", "2017-04-06", "1:30", testStylist1.getStylistId());
+    // testClient.save();
+    // Client testClient2 = new Client("Quin Tuplet", "2017-04-03", "4:50", testStylist1.getStylistId());
+    // testClient.save();
+    // Client testClient3 = new Client("Terry The Tank", "2017-04-16", "3:10", testStylist1.getStylistId());
+    // testClient3.save();
+    // Stylist testStylist2 = new Stylist("Joe Mathis", "Justics");
+    // testStylist2.save();
+    // Client testClient4 = new Client("Nameso Are Hardo", "2017-04-26", "6:30", testStylist2.getStylistId());
+    // testClient4.save();
+    // Client testClient5 = new Client("Thereaseaseasea", "2017-05-06", "5:00", testStylist2.getStylistId());
+    // testClient5.save();
+    // Client testClient6 = new Client("Mememe Meme", "2017-03-06", "4:30", testStylist2.getStylistId());
+    // testClient6.save();
+
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
