10c66642e8860ae8366ce13604cbe886a351c40c
slaughtr <dslaughtr@gmail.com>
2017-03-31 09:21:48 -0700

    Add basic getters and tests for stylist.java

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,4 +1,7 @@
 import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Stylist {
   private String name;
@@ -6,7 +9,7 @@ public class Stylist {
 
   private int id;
 
-  public Stylist(String name) {
+  public Stylist(String name, String specialty) {
     this.name = name;
     this.specialty = specialty;
 
@@ -14,6 +17,54 @@ public class Stylist {
 
   public String getStylistName() {
     return name;
+  }
+
+  public String getStylistSpecialty() {
+    return specialty;
+  }
+
+  public int getStylistId() {
+    return id;
+  }
+
+  public static List<Stylist> allStylists() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM stylists";
+      return con.createQuery(sql)
+      .executeAndFetch(Stylist.class);
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO stylists (name, specialty) VALUES (:name, :specialty)";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", this.name)
+      .addParameter("specialty", this.specialty)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  public static Stylist findStylist(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM stylists WHERE id = :id";
+      Stylist patient = con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetchFirst(Stylist.class);
+      return patient;
+    }
+  }
+
+
+  @Override
+  public boolean equals(Object otherStylist){
+    if (!(otherStylist instanceof Stylist)){
+      return false;
+    } else {
+      Stylist newStylist = (Stylist) otherStylist;
+      return this.getStylistName().equals(newStylist.getStylistName()) && this.getStylistId() == newStylist.getStylistId();
+    }
   }
 
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -7,5 +7,56 @@ public class StylistTest {
   @Rule
     public DatabaseRule database = new DatabaseRule();
 
+    @Test
+    public void client_instantiatesCorrectly_true() {
+      Stylist testStylist = new Stylist("Purse On", "Mohawks");
+      assertTrue(testStylist instanceof Stylist);
+    }
+
+    @Test
+    public void client_instantiatesWithName_true() {
+      Stylist testStylist = new Stylist("Purse On", "Mohawks");
+      assertTrue(testStylist.getStylistName().equals("Purse On"));
+    }
+
+    @Test
+    public void client_instantiatesWithSpecialty_true() {
+      Stylist testStylist = new Stylist("Purse On", "Mohawks");
+      assertTrue(testStylist.getStylistSpecialty().equals("Mohawks"));
+    }
+
+    @Test
+    public void client_instantiatesWithId_true() {
+      Stylist testStylist = new Stylist("Purse On", "Mohawks");
+      testStylist.save();
+      assertTrue(testStylist.getStylistId() > 0);
+    }
+
+    @Test
+    public void client_savesToDatabase_true() {
+      Stylist testStylist = new Stylist("Purse On", "Mohawks");
+      testStylist.save();
+      assertTrue(Stylist.findStylist(testStylist.getStylistId()).equals(testStylist));
+    }
+
+    @Test
+    public void all_ReturnsAllInstancesOfStylist_true() {
+      Stylist testStylist1 = new Stylist("Purse On", "Mohawks");
+      testStylist1.save();
+      Stylist testStylist2 = new Stylist("Not a Purse On", "Ponytails");
+      testStylist2.save();
+      assertTrue(Stylist.allStylists().get(0).equals(testStylist1));
+      assertTrue(Stylist.allStylists().get(1).equals(testStylist2));
+    }
+
+    @Test
+  public void find_returnsStylistWithSameId_secondStylist() {
+    Stylist testStylist1 = new Stylist("Purse On", "Mohawks");
+    testStylist1.save();
+    Stylist testStylist2 = new Stylist("Not a Purse On", "Ponytails");
+    testStylist2.save();
+    assertEquals(Stylist.findStylist(testStylist2.getStylistId()), testStylist2);
+  }
+
 
 }
